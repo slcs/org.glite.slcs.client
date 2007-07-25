8d2dcd900c9d3e8573fcd37a6159cac0fe784720
Valery Tschopp <valery.tschopp@switch.ch>
2007-07-25 15:37:17 +0000

    checks for ShibbolethClientMetadata element in config file.Apache 2.0 license.

--- src/org/glite/slcs/config/SLCSClientConfiguration.java
+++ src/org/glite/slcs/config/SLCSClientConfiguration.java
@@ -1,11 +1,21 @@
 /*
- * $Id: SLCSClientConfiguration.java,v 1.1 2006/10/24 09:24:18 vtschopp Exp $
- * 
- * Created on Aug 9, 2006 by tschopp
+ * Copyright (c) Members of the EGEE Collaboration. 2007.
+ * See http://www.eu-egee.org/partners/ for details on the copyright
+ * holders.
  *
- * Copyright (c) Members of the EGEE Collaboration. 2004.
- * See http://eu-egee.org/partners/ for details on the copyright holders.
- * For license conditions see the license file or http://eu-egee.org/license.html
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Version: $Id: SLCSClientConfiguration.java,v 1.2 2007/07/25 15:37:17 vtschopp Exp $
  */
 package org.glite.slcs.config;
 
@@ -19,93 +29,103 @@ import org.apache.commons.logging.LogFactory;
  * client.
  * 
  * @author Valery Tschopp <tschopp@switch.ch>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public class SLCSClientConfiguration extends SLCSConfiguration {
 
-    /** Logging */
-    private static Log LOG= LogFactory.getLog(SLCSClientConfiguration.class);
+	/** Logging */
+	private static Log LOG = LogFactory.getLog(SLCSClientConfiguration.class);
 
-    /** Singleton pattern */
-    private static SLCSClientConfiguration SINGLETON= null;
+	/** Singleton pattern */
+	private static SLCSClientConfiguration SINGLETON = null;
 
-    /**
-     * Factory pattern
-     * 
-     * @param filename
-     *            The XML configuration filename
-     * @return
-     * @throws SLCSConfigurationException
-     *             iff the configuration is not valid
-     */
-    public static synchronized SLCSClientConfiguration getInstance(
-            String filename) throws SLCSConfigurationException {
-        if (SINGLETON == null) {
-            SINGLETON= new SLCSClientConfiguration(filename);
-        }
-        return SINGLETON;
-    }
+	/**
+	 * Factory pattern
+	 * 
+	 * @param filename
+	 *            The XML configuration filename
+	 * @return
+	 * @throws SLCSConfigurationException
+	 *             iff the configuration is not valid
+	 */
+	public static synchronized SLCSClientConfiguration getInstance(
+			String filename) throws SLCSConfigurationException {
+		if (SINGLETON == null) {
+			SINGLETON = new SLCSClientConfiguration(filename);
+		}
+		return SINGLETON;
+	}
 
-    /**
-     * Constructor
-     * 
-     * @param filename
-     *            The XML config filename
-     * @throws SLCSConfigurationException
-     *             if an configuration error occurs
-     */
-    private SLCSClientConfiguration(String filename)
-            throws SLCSConfigurationException {
-        super(filename);
-    }
+	/**
+	 * Constructor
+	 * 
+	 * @param filename
+	 *            The XML config filename
+	 * @throws SLCSConfigurationException
+	 *             if an configuration error occurs
+	 */
+	private SLCSClientConfiguration(String filename)
+			throws SLCSConfigurationException {
+		super(filename);
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.glite.slcs.config.SLCSConfiguration#checkConfiguration()
-     */
-    protected void checkConfiguration() throws SLCSConfigurationException {
-        // HttpClient.TrustStoreFile
-        if (!contains("HttpClient.TrustStoreFile")) {
-            String filename= getFilename();
-            LOG.error("SLCSClientConfiguration(" + filename
-                    + "): HttpClient.TrustStoreFile element missing");
-            throw new SLCSConfigurationException("Element HttpClient.TrustStoreFile not defined in: "
-                    + filename);
-        }
-        // StoreDirectory
-        if (!contains("StoreDirectory")) {
-            String filename= getFilename();
-            LOG.error("SLCSClientConfiguration(" + filename
-                    + "): StoreDirectory element missing");
-            throw new SLCSConfigurationException("Element StoreDirectory not defined in: "
-                    + filename);
-        }
-        // StoreDirectory
-        if (!contains("UserKeyFile")) {
-            String filename= getFilename();
-            LOG.error("SLCSClientConfiguration(" + filename
-                    + "): UserKeyFile element missing");
-            throw new SLCSConfigurationException("Element UserKeyFile not defined in: "
-                    + filename);
-        }
-        // StoreDirectory
-        if (!contains("UserCertFile")) {
-            String filename= getFilename();
-            LOG.error("SLCSClientConfiguration(" + filename
-                    + "): UserCertFile element missing");
-            throw new SLCSConfigurationException("Element UserCertFile not defined in: "
-                    + filename);
-        }
-        // StoreDirectory
-        if (!contains("UserKeySize")) {
-            String filename= getFilename();
-            LOG.error("SLCSClientConfiguration(" + filename
-                    + "): UserKeySize element missing");
-            throw new SLCSConfigurationException("Element UserKeySize not defined in: "
-                    + filename);
-        }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.glite.slcs.config.SLCSConfiguration#checkConfiguration()
+	 */
+	protected void checkConfiguration() throws SLCSConfigurationException {
+		// HttpClient.TrustStoreFile
+		if (!contains("HttpClient.TrustStoreFile")) {
+			String filename = getFilename();
+			LOG.error("SLCSClientConfiguration(" + filename
+					+ "): HttpClient.TrustStoreFile element missing");
+			throw new SLCSConfigurationException(
+					"Element HttpClient.TrustStoreFile not defined in: "
+							+ filename);
+		}
+		// StoreDirectory
+		if (!contains("StoreDirectory")) {
+			String filename = getFilename();
+			LOG.error("SLCSClientConfiguration(" + filename
+					+ "): StoreDirectory element missing");
+			throw new SLCSConfigurationException(
+					"Element StoreDirectory not defined in: " + filename);
+		}
+		// UserKeyFile
+		if (!contains("UserKeyFile")) {
+			String filename = getFilename();
+			LOG.error("SLCSClientConfiguration(" + filename
+					+ "): UserKeyFile element missing");
+			throw new SLCSConfigurationException(
+					"Element UserKeyFile not defined in: " + filename);
+		}
+		// UserCertFile
+		if (!contains("UserCertFile")) {
+			String filename = getFilename();
+			LOG.error("SLCSClientConfiguration(" + filename
+					+ "): UserCertFile element missing");
+			throw new SLCSConfigurationException(
+					"Element UserCertFile not defined in: " + filename);
+		}
+		// UserKeySize
+		if (!contains("UserKeySize")) {
+			String filename = getFilename();
+			LOG.error("SLCSClientConfiguration(" + filename
+					+ "): UserKeySize element missing");
+			throw new SLCSConfigurationException(
+					"Element UserKeySize not defined in: " + filename);
+		}
+		// ShibbolethClientMetadata
+		if (getConfiguration().subset("ShibbolethClientMetadata").isEmpty()) {
+			String filename = getFilename();
+			LOG.error("SLCSClientConfiguration(" + filename
+					+ "): ShibbolethClientMetadata element missing");
+			throw new SLCSConfigurationException(
+					"Element ShibbolethClientMetadata not defined in: "
+							+ filename);
+		}
 
-    }
+	}
 
 }
