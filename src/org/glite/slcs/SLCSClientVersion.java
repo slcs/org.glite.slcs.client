d5e66d5d75ee4ceb95c60ac93e8d1d06669e95b1
Valery Tschopp <valery.tschopp@switch.ch>
2009-01-14 10:09:52 +0000

    Copyright updated. Version 1.3.4-1

--- src/org/glite/slcs/SLCSClientVersion.java
+++ src/org/glite/slcs/SLCSClientVersion.java
@@ -1,11 +1,19 @@
 /*
- * $Id: SLCSClientVersion.java,v 1.20 2008/08/12 07:50:08 vtschopp Exp $
- * 
- * Created on Aug 9, 2006 by tschopp
+ * Copyright (c) 2007-2009. Members of the EGEE Collaboration.
  *
- * Copyright (c) Members of the EGEE Collaboration. 2004.
- * See http://eu-egee.org/partners/ for details on the copyright holders.
- * For license conditions see the license file or http://eu-egee.org/license.html
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * $Id: SLCSClientVersion.java,v 1.21 2009/01/14 10:09:52 vtschopp Exp $
  */
 package org.glite.slcs;
 
@@ -16,12 +24,12 @@ public class SLCSClientVersion {
     /** Minor version number */
     static public final int MINOR= 3;
     /** Revision version number */
-    static public final int REVISION= 3;
+    static public final int REVISION= 4;
      /** Build number */
-    static public final int BUILD= 3;
+    static public final int BUILD= 1;
     
     /** Copyright */
-    static public final String COPYRIGHT= "Copyright (c) 2008. Members of the EGEE Collaboration";
+    static public final String COPYRIGHT= "Copyright (c) 2008-2009. Members of the EGEE Collaboration";
 
     /**
      * Prevents instantiation
